7035ec24b2bbb1ef7e3d75e6c60d0ae2dd98bb8f
vedantkadam <83024561+vedantkadam@users.noreply.github.com>
2022-03-02 00:17:51 +0530

    prepared debug for production

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,13 @@
+from django.conf.urls import url
+from django.conf import settings
+from django.views.static import serve
 from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
     path('', views.index),
     path('room/', views.room),
     path('lobby/', views.lobby),
